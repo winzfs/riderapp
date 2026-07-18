bcaa3b3b5b99f5aae0b209470081d910e3f3d390
winzfs <jazzhjm@gmail.com>
2026-07-18 17:55:29 +0900

    Test Kakao Navi destination parsing

--- app/src/test/java/com/winzfs/navcapture/parser/RawNavigationUriParserTest.kt
+++ app/src/test/java/com/winzfs/navcapture/parser/RawNavigationUriParserTest.kt
@@ -20,12 +20,24 @@ class RawNavigationUriParserTest {
     }
 
     @Test
-    fun parsesKakaoRouteDestination() {
+    fun parsesKakaoMapRouteDestination() {
         val result = parser.parse(
             "kakaomap://route?sp=35.1,126.8&ep=35.1595454,126.8526012&by=car",
         )
 
         assertEquals("route", result.host)
+        assertEquals(35.1595454, result.latitude!!, 0.0000001)
+        assertEquals(126.8526012, result.longitude!!, 0.0000001)
+    }
+
+    @Test
+    fun parsesKakaoNaviDestinationPayload() {
+        val payload = "%7B%22destination%22%3A%7B%22name%22%3A%22%EA%B4%91%EC%A3%BC%EA%B4%91%EC%97%AD%EC%8B%9C%EC%B2%AD%22%2C%22x%22%3A126.8526012%2C%22y%22%3A35.1595454%7D%2C%22option%22%3A%7B%22coord_type%22%3A%22wgs84%22%7D%7D"
+        val result = parser.parse("kakaonavi-sdk://navigate?param=$payload")
+
+        assertEquals("kakaonavi-sdk", result.scheme)
+        assertEquals("navigate", result.host)
+        assertEquals("광주광역시청", result.destinationName)
         assertEquals(35.1595454, result.latitude!!, 0.0000001)
         assertEquals(126.8526012, result.longitude!!, 0.0000001)
     }
