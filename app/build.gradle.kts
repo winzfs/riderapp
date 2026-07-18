718bbd0b67b7b4f85a497a6f2f24aee98dc10c2f
winzfs <jazzhjm@gmail.com>
2026-07-19 04:47:40 +0900

    Bump RiderApp version to 0.5.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.winzfs.navcapture"
         minSdk = 26
         targetSdk = 35
-        versionCode = 12
-        versionName = "0.5.7"
+        versionCode = 13
+        versionName = "0.5.8"
     }
 
     buildTypes {
